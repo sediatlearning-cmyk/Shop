25a3b0730c92fa8e7bd049735e55a1763a514e7c
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-09-24 13:44:35 +0200

    TestFindAll method
    Creation of the test method for the `findAll` method, including its
    invocation in the `main` method.

--- src/tests/TestsArticleDao.java
+++ src/tests/TestsArticleDao.java
@@ -23,10 +23,49 @@ public class TestsArticleDao {
 		
 		testsArticleDao.testCreateArticle();
 		testsArticleDao.testFindAll();
-		testsArticleDao.testFindById();
-		testsArticleDao.testUpdateArticle();
-		testsArticleDao.testDeleteArticle();
 
 	}
 	
+	/** 
+	 * Tests the creation of a new article. 
+	 * Creates an ArticleDao and an Article object, then calls 
+	 * the createArticle() method to insert the article into the database. 
+	 * The findById() method is then used to verify that the article 
+	 * has been successfully inserted. 
+	 */
+	public void testCreateArticle() {
+		
+		ArticleDao articleDao = new ArticleDao();
+		Article article = new Article(99, "LaptopTest", "Test", 599.99);
+		
+		articleDao.createArticle(article);
+		
+		System.out.println(articleDao.findById(99)); 
+		
+	}
 	
+	/** 
+	 * Tests the retrieval of all articles.  
+	 * Calls the findAll() method to retrieve the articles from the database. 
+	 * If the returned list is not null or empty, each article is displayed. 
+	 * Otherwise, a message indicating that no article was found is displayed. 
+	 */
+	public void testFindAll() {
+		
+		ArticleDao articleDao = new ArticleDao();
+		
+		List<Article> articles = articleDao.findAll();
+		
+		if(articles != null && !articles.isEmpty()) {
+			
+			for (Article article : articles) {
+				System.out.println(article);
+			}
+		}
+		else {
+			System.out.println("pas d'article trouvé");
+		}	
+	}
+
+	
+}
